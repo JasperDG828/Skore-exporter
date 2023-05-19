04513410fdf60beb7be9f90ae9b689c291396bde
Jasper De Gussemé <jasperdegus@gmail.com>
2023-05-19 09:23:15 +0200

    Repeated button check, code comments

--- script.js
+++ script.js
@@ -1,60 +1,82 @@
-setTimeout(() => {
+setInterval(() => { checkButtons() }, 500) // Each interval, check wether the buttons need to be loaded
+
+function setButtons() {
+    // Locate destination Div
     let div = document.getElementsByClassName("wide-toolbar js-wide-toolbar")[0]
+
+    // Set Export to JSON Button
     let buttonJSON = document.createElement("button")
-    buttonJSON.style="background-color: rgba(0,0,0,0); border: none;"
+    buttonJSON.className = "skore-exporter"
+    buttonJSON.style = "background-color: rgba(0,0,0,0); border: none;"
     buttonJSON.innerHTML = '<img src="https://cdn-icons-png.flaticon.com/512/136/136525.png" height=50><br><p onmouseover="this.style.textDecoration=\'underline\';" onmouseout="this.style.textDecoration=\'none\';">Export to JSON</p>'
     buttonJSON.addEventListener("click", () => {
         exportScore('json')
     })
+    // Set Export to CSV Button
     let buttonCSV = document.createElement("button")
-
-    buttonCSV.style="background-color: rgba(0,0,0,0); border: none;"
+    buttonCSV.className = "skore-exporter"
+    buttonCSV.style = "background-color: rgba(0,0,0,0); border: none;"
     buttonCSV.innerHTML = '<img src="https://cdn-icons-png.flaticon.com/512/136/136534.png" height=50><br><p onmouseover="this.style.textDecoration=\'underline\';" onmouseout="this.style.textDecoration=\'none\';">Export to CSV</p>'
     buttonCSV.addEventListener("click", () => {
         exportScore('csv')
     })
-    
 
+    // Append buttons to the div
     div.appendChild(buttonJSON)
     div.appendChild(buttonCSV)
-}, 500)
+}
 
-function exportScore(format){
+function checkButtons() {
+    // If the buttons are gone and we're on the right page, place the buttons
+    if (document.getElementsByClassName("skore-exporter").length == 0 && window.location["href"].includes("/results/main/results")) {
+        setButtons()
+    }
+}
+
+function exportScore(format) {
+    // Get tests from api
     fetch('https://svi-gijzegem.smartschool.be/results/api/v1/evaluations/?pageNumber=1&itemsOnPage=300').then((res) => {
-            return res.json()
-        }).then((data) => {
-            let tests = []
-            data.forEach(el => {
-                const id = el["identifier"]
-                if (el["type"]=="normal"){
-                    score = el["graphic"]["description"].replace(",", ".").split("/")
-                    let n = parseFloat(score[0])
-                    let d = parseFloat(score[1])
-                    tests.push({"name": el["name"], "scoreN": n, "scoreD": d, "percentage": el["graphic"]["value"], "teacher": el["gradebookOwner"]["name"]["startingWithFirstName"], "published": el["availabilityDate"], "course": el["courses"][0]["name"]})
-                }
-                
+        return res.json()
+    }).then((data) => {
+        let tests = []
+        // For each test in api-return, create a new test in tests-array with right values
+        data.forEach(el => {
+            const id = el["identifier"]
+            if (el["type"] == "normal") {
+                score = el["graphic"]["description"].replace(",", ".").split("/")
+                let n = parseFloat(score[0])
+                let d = parseFloat(score[1])
+                tests.push({ "name": el["name"], "scoreN": n, "scoreD": d, "percentage": el["graphic"]["value"], "teacher": el["gradebookOwner"]["name"]["startingWithFirstName"], "published": el["availabilityDate"], "course": el["courses"][0]["name"] })
+            }
+
+        });
+        // Create link that downloads the file when clicked
+        var element = document.createElement('a');
+        
+        // Set link destination to the JSON file
+        if (format == 'json') {
+            element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(tests)));
+        }
+        // Set link destination to the CSV file
+        else if (format == 'csv') {
+            // Generate CSV
+            let csv = "Name,ScoreN,ScoreD,Percentage,Teacher,Published,Course"
+            tests.forEach(el => {
+                csv = csv + `\n"${el["name"]}",${el["scoreN"]},${el["scoreD"]},${el["percentage"]},"${el['teacher']}",${el['published']},"${el['course']}"`;
             });
-            //Download
-            var element = document.createElement('a');
-            if (format=='json'){
-                element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(tests)));
-            }
-            else if (format=='csv'){
-                let csv = "Name,ScoreN,ScoreD,Percentage,Teacher,Published,Course"
-                tests.forEach(el => {
-                    csv=csv+`\n"${el["name"]}",${el["scoreN"]},${el["scoreD"]},${el["percentage"]},"${el['teacher']}",${el['published']},"${el['course']}"`;
-                });
-                element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(csv));
-            }
-            
-            
-            element.setAttribute('download', "skore."+format);
+            element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(csv));
+        }
+
+        // Set download name
+        element.setAttribute('download', "skore." + format);
+
+        // Add the link to the DOM
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        // Click the link and remove it from the DOM
+        element.click();
+        document.body.removeChild(element);
         
-            element.style.display = 'none';
-            document.body.appendChild(element);
-        
-            element.click();
-        
-            document.body.removeChild(element);
-        }).catch((err) => { console.error(err) });
+    }).catch((err) => { console.error(err) });
 }
